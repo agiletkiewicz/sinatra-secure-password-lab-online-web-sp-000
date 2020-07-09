c55e4ecef9c0f7e6334a4e83b0e83824e0e0a6c6
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-09 20:38:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,12 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate
+      session[:user_id] = @user.id 
+      redirect to '/account'
+    else 
+      redirect to 'failure'
+    end
   end
 
   get "/failure" do
